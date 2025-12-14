27f847a80a4cd07ea4c18b1547e61a2ad729d895
lakabd <lakbir.abder@gmail.com>
2025-12-14 23:35:13 +0100

    [#9] Display: add constructor

--- src/display.cpp
+++ src/display.cpp
@@ -20,17 +20,58 @@
  * SOFTWARE.
  */
 
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "display.hpp"
 
 
 Display::Display(bool verbose)
-    : m_drmFd(-1), m_drmRes(nullptr), m_drmConnector(nullptr), m_drmEncoder(nullptr), m_drmCrtc(nullptr),
-     m_gbmDev(nullptr), m_bo(nullptr), m_gbmSurface(nullptr), m_logger("display", verbose)
+    : m_drmFd(-1), m_drmRes(nullptr), m_drmConnector(nullptr), m_drmEncoder(nullptr), m_drmCrtc(nullptr), m_gbmDev(nullptr), m_bo(nullptr), 
+    m_gbmSurface(nullptr), m_logger("display", verbose)
 {
-    
+    Logger& log = m_logger;
+
+    // Open DRM device
+    const char* drmDevices[] = {"/dev/dri/card0", "/dev/dri/card1", "/dev/dri/renderD128"};
+    for(const char* device : drmDevices){
+        m_drmFd = open(device, O_RDWR);
+        if(m_drmFd >= 0){
+            log.info("Using DRM device: %s", device);
+            break;
+        }
+    }
+    if(m_drmFd < 0)
+        log.fatal("No DRM device found !");
+
+    // Create GBM device
+    m_gbmDev = gbm_create_device(m_drmFd);
+    if(!m_gbmDev){
+        log.fatal("Failed to create GBM device");
+    }
+    log.info("GBM backend: %s", gbm_device_get_backend_name(m_gbmDev));
+}
+
+bool Display::initialize()
+{
+    //TODO
+    return true;
+}
+
+bool Display::scanout()
+{
+    //TODO
+    return true;
 }
 
 Display::~Display()
 {
+    Logger& log = m_logger;
+    log.status("Quitting...");
     
+    // Distro GBM device
+    gbm_device_destroy(m_gbmDev);
+    // Close DRM fd
+    close(m_drmFd);
 }
